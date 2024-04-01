313a996d651954c44a41bbf0c8b6380dcebecfbf
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-02 00:30:36 +0800

    Refactor product model to use Sequelize

--- models/product.js
+++ models/product.js
@@ -1,30 +1,28 @@
-const db = require("../util/database");
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../util/database");
+const Product = sequelize.define("product", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    defaultValue: 0.0,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    defaultValue:
+      "https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg",
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+});
 
-const Cart = require("./cart");
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE id = ?", [id]);
-  }
-};
+module.exports = Product;
