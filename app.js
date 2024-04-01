28777f3f72d62c8b8c9393ed300befb9ec6e9d94
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-02 01:34:48 +0800

    Refactor sequelize sync and add error handling

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const sequelize = require('./util/database');
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -20,6 +20,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync();
-
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    //console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
