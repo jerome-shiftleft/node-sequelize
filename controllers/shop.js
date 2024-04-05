7d31e5e172375fed8eab817abd363ccba25f6c18
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-06 06:41:10 +0800

    Refactor getProducts function to use Sequelize in shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,17 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/product-list", {
-        prods: rows,
+        prods: products,
         pageTitle: "All Products",
         path: "/products",
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
