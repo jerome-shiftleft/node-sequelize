1b4b7391f15775e881a3ad05938d06c4a9feb04e
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-21 14:28:54 +0800

    progress

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
